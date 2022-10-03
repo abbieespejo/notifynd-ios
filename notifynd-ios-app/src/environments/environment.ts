607115f0a01c18ea396a2ec184e108dec8d368c8
espejoabig <espejoabig@myvuw.ac.nz>
2022-10-03 22:48:27 +1300

    Successfully connected Firestore db

--- notifynd-ios-app/src/environments/environment.ts
+++ notifynd-ios-app/src/environments/environment.ts
@@ -3,6 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
+  firebase: {
+    projectId: 'notifynd-9867d',
+    appId: '1:263574213360:web:2f35f82e90a1f0fbaba835',
+    storageBucket: 'notifynd-9867d.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyDG0tVivv0bIVHeopXu13rKQ8V0Jzu6TO0',
+    authDomain: 'notifynd-9867d.firebaseapp.com',
+    messagingSenderId: '263574213360',
+  },
   production: false,
 };
 
