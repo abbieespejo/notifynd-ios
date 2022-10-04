b25496a267de9997ddb1f3e83ac1c418db332582
espejoabig <espejoabig@myvuw.ac.nz>
2022-10-04 20:24:03 +1300

    Testing notification triggering

--- notifynd-ios-app/src/app/tab2/tab2.page.ts
+++ notifynd-ios-app/src/app/tab2/tab2.page.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { collection, collectionData, DocumentData, Firestore } from '@angular/fire/firestore';
 import { Geolocation } from '@awesome-cordova-plugins/geolocation/ngx';
-//import { LocalNotifications } from '@awesome-cordova-plugins/local-notifications/ngx';
+import { LocalNotifications } from '@awesome-cordova-plugins/local-notifications/ngx';
+import { Diagnostic } from '@awesome-cordova-plugins/diagnostic/ngx';
 import { DataService } from '../services/data.service';
-
 declare var cordova: any;
+
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
@@ -19,59 +20,90 @@ export class Tab2Page {
 
   collection = [];
 
-  constructor(private geolocation: Geolocation, private db: DataService) { }
+  constructor(private geolocation: Geolocation,
+    private db: DataService,
+    private localNotif: LocalNotifications, 
+    private diagnostic: Diagnostic) { }
 
   prepareNotifications() {
-    cordova.plugins.notification.local.schedule({
+    let notifCollection = this.db.getNotifsInDB();
+    notifCollection.subscribe(docs => {
+      for (let i = 0; i <= docs.length; i++) {
+        console.log("id: " + i + "\n" +
+        "title: " + docs[i].title + "\n" +
+        "text: " + docs[i].title + "\n" +
+        "center: " + docs[i].latitude + ", " + docs[i].longitude + "\n" +
+        "radius: " + docs[i].radius + "\n");
+        this.localNotif.schedule({
+          id: i,
+          title: docs[i].title,
+          text: docs[i].text,
+          foreground: true,
+          trigger: {
+            // type: 'location',
+            center: [docs[i].latitude, docs[i].longitude],
+            radius: docs[i].radius,
+            notifyOnEntry: true
+          }
+        });
+      }
+    });
+  }
+
+  testNotif() {
+    this.localNotif.schedule(
+      {
       title: 'My first notification',
       text: 'Thats pretty easy...',
       foreground: true
-  });
-    // let notifCollection = this.db.getNotifsInDB();
-    // notifCollection.subscribe(docs => {
-    //   for (let i = 0; i <= docs.length; i++) {
-    //     this.localNotif.schedule({
-    //       id: i,
-    //       title: docs[i].title,
-    //       text: docs[i].text,
-    //       trigger: {
-    //         type: 'location',
-    //         center: [docs[i].latitude, docs[i].longitude],
-    //         radius: docs[i].radius,
-    //         notifyOnEntry: true
-    //       }
-    //     });
-    //   }
-    // });
-    // this.localNotif.getAllScheduled().then(result => {
-    //   console.log("THESE ARE THE SCHEDULED NOTIFS" + result);
-    }
+      });
+    // this.localNotif.requestPermission();
+  //   cordova.plugins.notification.local.schedule([
+  //     {
+  //     title: 'My first notification',
+  //     text: 'Thats pretty easy...',
+  //     foreground: true
+  //     },
+  //     {
+  //     title: 'Welcome to 10 Moana Road',
+  //     foreground: true,
+  //     trigger: {
+  //         type: 'location',
+  //         center: [-41.287901, 174.754702], // The center point of the geographic area.
+  //         radius: 3, // The radius (measured in meters) that defines the geographic area’s outer boundary.
+  //         notifyOnEntry: true
+  //     }
+  //   }
 
+}
   
   getCoordinates() {
-    this.geolocation.getCurrentPosition().then((resp) => {
+    // this.geolocation.getCurrentPosition().then((resp) => {
 
-      this.locationTraces.push({
-        latitude:resp.coords.latitude,
-        longitude:resp.coords.latitude,
-        accuracy:resp.coords.accuracy,
-        timestamp:resp.timestamp
-      });
-    }).catch((error) => {
-      console.log('Error getting location', error);
-    });
+    //   this.locationTraces.push({
+    //     latitude:resp.coords.latitude,
+    //     longitude:resp.coords.latitude,
+    //     accuracy:resp.coords.accuracy,
+    //     timestamp:resp.timestamp
+    //   });
+    // }).catch((error) => {
+    //   console.log('Error getting location', error);
+    // });
 
     this.locationSubscription = this.geolocation.watchPosition();
     this.locationSubscription.subscribe((resp) => {
 
       this.locationWatchStarted = true;
-      this.locationTraces.push({
-        latitude:resp.coords.latitude,
-        longitude:resp.coords.latitude,
-        accuracy:resp.coords.accuracy,
-        timestamp:resp.timestamp
-      });
-
+      // this.locationTraces.push({
+      //   latitude:resp.coords.latitude,
+      //   longitude:resp.coords.latitude,
+      //   accuracy:resp.coords.accuracy,
+      //   timestamp:resp.timestamp
+      // });
+      console.log("\n Current location: \n" +
+        "Latitude: " + resp.coords.latitude + "\n" +
+        "Longitude: " + resp.coords.longitude + "\n" +
+        "------------------------------------------");
     });
   }
 
