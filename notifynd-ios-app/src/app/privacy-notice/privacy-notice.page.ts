69167cf4fb881790c836cbabdb3befc63ebd9dbd
espejoabig <espejoabig@myvuw.ac.nz>
2022-10-05 23:14:38 +1300

    Restructuring and designing app

--- notifynd-ios-app/src/app/privacy-notice/privacy-notice.page.ts
+++ notifynd-ios-app/src/app/privacy-notice/privacy-notice.page.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Diagnostic } from '@awesome-cordova-plugins/diagnostic/ngx';
+import { DataService } from '../services/data.service';
+import { LocalNotifications } from '@awesome-cordova-plugins/local-notifications/ngx';
 
 
 @Component({
@@ -10,20 +12,53 @@ import { Diagnostic } from '@awesome-cordova-plugins/diagnostic/ngx';
 
 export class PrivacyNoticePage {
 
-  buttonDisabled: boolean = true;
+  locationButtonDisabled: boolean = true;
 
-  constructor(private diagnostic: Diagnostic) {
+  constructor(private firestoreDB: DataService, private diagnostic: Diagnostic, private localNotif: LocalNotifications) {
    
   }
  
   allowLocationTracking() {
     let successCallback = (authStatus) => {
         if(authStatus === this.diagnostic.permissionStatus.GRANTED_WHEN_IN_USE || this.diagnostic.permissionStatus.GRANTED) {
-          this.buttonDisabled = false;
+          this.locationButtonDisabled = false;
         }
       }
     let errorCallback = (e) => console.log(e);
     this.diagnostic.requestLocationAuthorization().then(successCallback).catch(errorCallback);
+  }
+
+   /**
+   * Checks if the app is authorised to send notifications. If not,
+   * app requests permission to send notifications.
+   * 
+   * Then, reads notification documents from Firestore database and
+   * schedules them as location-based notifications. Notifications will only
+   * be triggered if the device's coordinates are within the radius.
+   */
+  prepareNotifications() {
+    this.localNotif.hasPermission().then(result => {
+      if(!result) {
+        this.localNotif.requestPermission();
+      }
+    });
+    let notifCollection = this.firestoreDB.getNotifsInDB();
+    notifCollection.subscribe(docs => {
+      for (let i = 0; i <= docs.length; i++) {
+        this.localNotif.schedule({
+          id: i,
+          title: docs[i].title,
+          text: docs[i].text,
+          foreground: true,
+          trigger: {
+            type: 'location',
+            center: [docs[i].latitude, docs[i].longitude],
+            radius: docs[i].radius,
+            notifyOnEntry: true
+          }
+        });
+      }
+    });
   }
 }
    
--- notifynd-ios-app/src/app/tab1/tab1.page.ts
+++ notifynd-ios-app/src/app/tab1/tab1.page.ts
@@ -1,12 +1,55 @@
 import { Component } from '@angular/core';
+import { LocalNotifications } from '@awesome-cordova-plugins/local-notifications/ngx';
+import { DataService } from '../services/data.service';
+
  
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
+
 export class Tab1Page {
 
-  constructor() {}
+  buttonDisabled: boolean = false;
+
+  constructor(private localNotif: LocalNotifications, private firestoreDB: DataService) {}
+
+  
+ 
+  prepareNotifications() {
+    let notifCollection = this.firestoreDB.getNotifsInDB();
+    notifCollection.subscribe(docs => {
+      for (let i = 0; i <= docs.length; i++) {
+        this.localNotif.schedule({
+          id: i,
+          title: docs[i].title,
+          text: docs[i].text,
+          foreground: true,
+          trigger: {
+            // type: 'location',
+            center: [docs[i].latitude, docs[i].longitude],
+            radius: docs[i].radius,
+            notifyOnEntry: true
+          }
+        });
+      }
+    });
+  }
+
+  checkNotifications() {
+    this.localNotif.getAllScheduled().then(data => {
+      for(let i = 0; i <= data.length; i++) {
+      console.log("\nRETRIEVED NOTIFICATION: " + data[i].id +"\n" +
+      "Title: " + data[i].title + "\n" +
+      "Text: " + data[i].text + "\n" + 
+      "Trigger details:\n" +
+      "Latitude: " + data[i].trigger.center[0] + "\n" +
+      "Longitude: " + data[i].trigger.center[1] + "\n" +
+      "Radius: " + data[i].trigger.radius + "\n" +
+      "Notify on entry: " + data[i].trigger.notifyOnEntry + "\n");
+      }
+    });
+  }
 
 }
